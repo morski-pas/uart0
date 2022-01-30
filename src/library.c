cbabe564607482fb07ce6c341a0ca6c74d5c745a
simon <simon.pechlaner@protonmail.com>
2022-01-30 13:24:05 +0100

    added source file

---
+++ src/library.c
@@ -0,0 +1,94 @@
+#include "uart0.h" // is located in same directory, therefore "*.h"
+#include <avr/interrupt.h>
+
+void (*pRecepionCallback)(uint8_t data) = NULL; // null indicated that no callback function has been registered yet
+
+
+void uart0_init(uint32_t baudrate, enum UartMode mode, enum UartParity parity)
+{
+	UCSR0A = 0x00;
+	UCSR0B = 0x00; 
+	
+	switch(mode)
+	{
+		case UART_M_RECEIVE:
+		{
+			UCSR0B |= (1 << RXEN0);
+			break;
+		}
+
+		case UART_M_TRANSMIT:
+		{
+			UCSR0B |= (1 << TXEN0);
+			break;
+		}
+
+		case UART_M_TRANSCEIVE:
+		{
+			UCSR0B |= ((1 << RXEN0) | (1 << TXEN0));
+			break;
+		}
+
+		default: break;
+	}
+
+	UCSR0C = ((1 << USBS0) | (0b11 << UCSZ00));
+
+	switch(parity)
+	{
+		case UART_P_NONE: break;
+		case UART_P_EVEN: UCSR0C |= (0b10 << UPM00); break;
+		case UART_P_ODD:  UCSR0C |= (0b11 << UPM00); break;
+	}
+
+	UBRR0 = F_CPU / (16 * baudrate) - 1;
+}
+
+
+uint8_t uart0_getc()
+{
+	while(!(UCSR0A & (1 << RXC0)));
+	return UDR0;
+}
+
+
+void uart0_putc(char c)
+{
+	// send c to uart0
+	while(!(UCSR0A & (1 << UDRE0)));
+	UDR0 = c;
+}
+
+
+void uart0_puts(char* _str)
+{
+	while(*_str != '\0')
+	{
+		uart0_putc(*_str);
+		_str++;
+	}
+}
+
+
+
+void uart0_registerReceptionCallback(void (*pCallback)(uint8_t data))
+{
+    sei();
+    
+    if(pCallback)
+        UCSR0B |= (1 << RXCIE0); // enable reception callback
+
+    else
+        UCSR0B &= ~(1 << RXCIE0); // disable reception callback
+    
+    pRecepionCallback = pCallback;
+}
+
+
+ISR(USART0_RX_vect)
+{
+    if(pRecepionCallback)
+    {
+        (*pRecepionCallback)(UDR0);
+    }
+}
